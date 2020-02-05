c902552618df1c3ac57d95f84a5641e907b41b1c
Mario de León <46909802+DavidDLM@users.noreply.github.com>
2020-02-05 16:10:17 -0600

    Add files via upload

---
+++ CalculadoraStack/src/iCalculadora.java
@@ -0,0 +1,18 @@
+/**
+ * @author Mario de Leon
+ * @author Mariana Morales
+ *
+ */
+public interface iCalculadora {
+
+
+	public int sumar(int a, int b); //Sumar
+	
+	public int restar(int a, int b); //Restar
+	
+	public int multiplicar(int a, int b); //Multiplicar
+	
+	public int dividir(int a, int b); //Dividir
+
+
+}
