150db5c719194fb0a1545a4c52d7e7ebd2faceab
Mario de León <46909802+DavidDLM@users.noreply.github.com>
2020-02-05 22:43:03 -0600

    Update iCalculadora.java

--- CalculadoraStack/src/iCalculadora.java
+++ CalculadoraStack/src/iCalculadora.java
@@ -5,16 +5,42 @@
  */
 public interface iCalculadora {
 
-
+	/** 
+	 * Suma dos enteros
+	 * @param a
+	 * @param b
+	 * @return resultado
+	 */
 	public int sumar(int a, int b); //Sumar
 	// pre: entran dos enteros
 	   // post: suma los enteros y devuelve el resultado
+	
+	/** 
+	 * Resta dos enteros
+	 * @param a
+	 * @param b
+	 * @return resultado
+	 */
 	public int restar(int a, int b); //Restar
 	// pre: entran dos enteros
 	   // post: resta los enteros y devuelve el resultado
+	
+	/** 
+	 * Multiplica dos enteros
+	 * @param a
+	 * @param b
+	 * @return resultado
+	 */
 	public int multiplicar(int a, int b); //Multiplicar
 	// pre: entran dos enteros
 	   // post: multiplica los enteros y devuelve el resultado
+	
+	/** 
+	 * Divide dos enteros
+	 * @param a
+	 * @param b
+	 * @return resultado
+	 */
 	public int dividir(int a, int b); //Dividir
 	// pre: entran dos enteros
 	   // post: divide los enteros y devuelve el resultado
